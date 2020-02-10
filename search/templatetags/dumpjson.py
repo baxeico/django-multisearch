a98e51109284dfbd29a1de8f84b5bfa0cea05153
Augusto Destrero <a.destrero@gmail.com>
2020-02-10 09:46:17 +0100

    use DjangoJSONEncoder in dumpjson template tag

--- search/templatetags/dumpjson.py
+++ search/templatetags/dumpjson.py
@@ -1,8 +1,9 @@
 import json
 from django import template
+from django.core.serializers.json import DjangoJSONEncoder
 
 register = template.Library()
 
 @register.filter
 def dumpjson(value):
-    return json.dumps(value)
+    return json.dumps(value, cls=DjangoJSONEncoder)
