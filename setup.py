c361d955d96a6345c1361d39e7f39d6d99e47fcd
Augusto Destrero <a.destrero@gmail.com>
2020-02-10 10:03:41 +0100

    version bump

--- setup.py
+++ setup.py
@@ -2,14 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name = 'django-search',
-    version = '1.1.0',
+    version = '1.1.1',
     packages = find_packages(),
     author = 'Augusto Destrero',
     author_email = 'a.destrero@gmail.com',
     license='MIT',
-    description = '',
+    description = 'Multifield search for Django, using Bootstrap 3 and jQuery DataTables',
     url = 'https://github.com/baxeico/django-search',
-    download_url = 'https://github.com/baxeico/django-search/archive/0.1.tar.gz',
     keywords = ['django', 'search'],
     include_package_data = True,
     zip_safe=False,
